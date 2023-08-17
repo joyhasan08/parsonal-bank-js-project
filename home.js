e4fe6000b79b351e76af0f79e16bba0a0801a452
Zarjij Hasan joy <rubayedhasan235@gmail.com>
2023-08-18 00:58:55 +0600

    js total sum added

--- home.js
+++ home.js
@@ -1,13 +1,28 @@
 
 //verible declar part
- let totalOfDeposit =0;
- let Total = totalOfDeposit.toString;
+ 
+//  let Total = totalOfDeposit.toString;
 
 document.getElementById('deposit-btn').addEventListener('click',function(){
     const getDeposit = document.getElementById('get-deposit').value;
     let intOfDeposit = parseInt(getDeposit)
-    totalOfDeposit += intOfDeposit;
+    let previousDeposit = document.getElementById('Deposit-display').innerText;
+    let previousDepositNumber = parseInt(previousDeposit);
+    // totalOfDeposit += intOfDeposit;
+    let totalOfDeposit = 0;
+    totalOfDeposit = intOfDeposit + previousDepositNumber;
 
-    document.getElementById('Deposit-display').innerText = Total ;
+    document.getElementById('Deposit-display').innerText = totalOfDeposit ;
     document.getElementById('get-deposit').value='';
+
+    // total blalance part 
+    let previousBalance = parseInt(document.getElementById('balance-display').innerText) 
+    let totalBalance = 0;
+    totalBalance = previousBalance + intOfDeposit;
+
+    document.getElementById('balance-display').innerText = totalBalance;
+
 })
+
+
+
